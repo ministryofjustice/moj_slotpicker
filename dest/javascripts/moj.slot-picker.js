823b9a0024d76c622587901fd414726c6b7ccf06
Mat Harden <matharden@gmail.com>
2014-03-25 15:34:10 +0000

    Remove MOJ tabs module from mark-up

--- dest/javascripts/moj.slot-picker.js
+++ dest/javascripts/moj.slot-picker.js
@@ -37,7 +37,6 @@
       this.$removeSlots = '.js-remove-slot';
       this.$promoteSlots = '.js-promote-slot';
       this.$promoteHelp = $('.js-promote-help');
-      this.$months = $('.js-slotpicker__months');
       this.$next = $('.BookingCalendar-nav .next');
       this.$prev = $('.BookingCalendar-nav .prev');
       this.$availableMonths = $('.BookingCalendar-availableMonths a');
@@ -72,10 +71,6 @@
         e.preventDefault();
         self.selectDay( $(this) );
         $('.js-slotpicker').addClass( 'is-active' );
-        
-        if (e.type !== 'chosen') {
-          self.confirmVisibility(self.$months);
-        }
       });
 
       this.$next.on('click', function(e) {
@@ -308,30 +303,6 @@
       $('[data-date=' + day + ']')[~this.settings.currentSlots.join('-').indexOf(day) ? 'addClass' : 'removeClass']('is-chosen');
     },
 
-    confirmVisibility: function($el) {
-      if (!this.isElementInViewport($el.get(0))) {
-        this.moveIntoViewport($el);
-      }
-    },
-
-    isElementInViewport: function(el) {
-      var rect = el.getBoundingClientRect();
-
-      return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
-      );
-    },
-
-    moveIntoViewport: function($el) {
-      var bottom = $el.offset().top + $el.height();
-      
-      $('html,body').animate({
-        scrollTop: bottom - $(window).height()
-      }, 350);
-    }
   };
 
 
